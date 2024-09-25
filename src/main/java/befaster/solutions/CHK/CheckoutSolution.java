8b6c34d13b509c9ee37c87579583ba16797e1f63
sbx_user1051 <sbx_user1051@169.254.46.61>
2024-09-25 23:31:33 +0000

    Wed Sep 25 23:31:33 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,19 +2,37 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class CheckoutSolution {
 
-    private static final Map<Character, Integer> price_table
-
-
+    private static final Map<Character, Integer> price_table = Map.of(
+            'A', 50, 'B', 30, 'C', 20, 'D', 15
+    );
 
     public Integer checkout(String skus) {
 
+        Integer itemCounterA = null;
+        Integer itemCounterB = null;
+        Integer itemCounterC = null;
+        Integer itemCounterD = null;
+
+        for(char sku : skus.toCharArray()){
+            if(sku == price_table.get(0)){
+                itemCounterA++;
+            }else if(sku == price_table.get(1)){
+                itemCounterB++;
+            }else if(sku == price_table.get(2)){
+                itemCounterC++;
+            }else if(sku == price_table.get(3)){
+                itemCounterD++;
+            }
+        }
 
 
-
+        
     }
 }
+
 
